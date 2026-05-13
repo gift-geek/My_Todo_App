889bfbabda723b7eb131d3c78cd02fddf0cb9336
gift-geek <giftnwosu881@gmail.com>
2026-05-13 01:06:42 +0100

    fix cors

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ mongoose.connect(live_url)
 .catch((err)=> console.error("Connection error",err));
 
 const app =express();
-app.use(cors());
+app.use(cors({
+    origin : "*"
+}));
 app.use(express.json());
 app.use("/todos", router);
 
